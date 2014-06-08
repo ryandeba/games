67d0bfa1523ad505c232e0f1bcd21ae9c3b625c4
ryandeba <ryandeba@gmail.com>
2014-06-08 14:40:47 -0500

    progress on settlers

--- src/games/apps/settlersOfCatan/models.py
+++ src/games/apps/settlersOfCatan/models.py
@@ -1,7 +1,7 @@
+import random
+
 from django.db import models
 from django.contrib.auth.models import User
-
-import random
 
 STATUS = {
     'PENDING' : 'P',
@@ -20,9 +20,9 @@ TILE_TYPE = {
 
 RESOURCE_TYPE = {
     'ORE' : 'OR',
-    'WHEAT' : 'WH',
-    'WOOD' : 'WO',
-    'SHEEP' : 'SH',
+    'GRAIN' : 'GR',
+    'LUMBER' : 'LU',
+    'WOOL' : 'WO',
     'BRICK' : 'BR',
 }
 
@@ -42,10 +42,26 @@ COLOR = {
 }
 
 class Game(models.Model):
-    status = models.CharField(max_length = 1, choices = ((value, key) for key, value in STATUS.items()))
-    player_largest_army = models.ForeignKey('Player', related_name = '+', null = True)
-    player_longest_road = models.ForeignKey('Player', related_name = '+', null = True)
-    tile_robber = models.ForeignKey('Tile', related_name = '+', null = True)
+    status = models.CharField(
+        max_length = 1,
+        choices = ((value, key) for key, value in STATUS.items()),
+        default = STATUS['PENDING']
+    )
+    player_largest_army = models.ForeignKey(
+        'Player',
+        related_name = '+',
+        null = True
+    )
+    player_longest_road = models.ForeignKey(
+        'Player',
+        related_name = '+',
+        null = True
+    )
+    tile_robber = models.ForeignKey(
+        'Tile',
+        related_name = '+',
+        null = True
+    )
     date_created = models.DateTimeField(auto_now_add = True)
     date_modified = models.DateTimeField(auto_now = True)
 
@@ -72,27 +88,53 @@ class Game(models.Model):
                    ---"""
 
     def add_player(self, user = None):
-        #if self.status != STATUS['PENDING'] and self.player_set.count() < 4:
-        if user == None:
-            return self.player_set.create(game = self)
-        else:
-            return self.player_set.get_or_create(game = self, user = user)
+        if self.status == STATUS['PENDING'] and self.player_set.count() < 4:
+            if user == None:
+                return self.player_set.create(game = self)
+            else:
+                return self.player_set.get_or_create(
+                    game = self, user = user
+                )[0]
 
     def start(self):
         self._generate_board()
         self._distribute_tokens()
+        self._generate_development_cards()
+        self._generate_resource_cards()
         self.status = STATUS['ACTIVE']
-        self.save()
+        self.start_next_round()
+
+    def start_next_round(self):
+        self.round_set.create(
+            game = self,
+            player = self._get_next_player_turn()
+        )
+
+    def _get_next_player_turn(self):
+        #TODO: building phase
+        if self.round_set.count() == 0:
+           return self.player_set.first()
+        player_last_round = self.round_set.last().player
+        return_next = False
+        for player in self.player_set.all():
+            if return_next:
+                return player
+            if player == player_last_round:
+                return_next = True
+        return self.player_set.first()
+
 
     def _generate_board(self):
-        tile_types = [TILE_TYPE['HILLS']] * 3 + [TILE_TYPE['PASTURE']] * 4 + [TILE_TYPE['MOUNTAINS']] * 3 + [TILE_TYPE['FIELDS']] * 4 + [TILE_TYPE['FOREST']] * 4 + [TILE_TYPE['DESERT']]
+        tile_types = [TILE_TYPE['HILLS']] * 3
+        tile_types = tile_types + [TILE_TYPE['PASTURE']] * 4
+        tile_types = tile_types + [TILE_TYPE['MOUNTAINS']] * 3
+        tile_types = tile_types + [TILE_TYPE['FIELDS']] * 4
+        tile_types = tile_types + [TILE_TYPE['FOREST']] * 4
+        tile_types = tile_types + [TILE_TYPE['DESERT']]
         chits = [2,3,3,4,4,5,5,6,6,8,8,9,9,10,10,11,11,12]
-        coordinates = [
-            (1, 2), (1, 3), (1, 4),
-            (2, 1), (2, 2), (2, 3), (2, 4),
-            (3, 1), (3, 2), (3, 3), (3, 4), (3, 5),
-            (4, 1), (4, 2), (4, 3), (4, 4),
-            (5, 2), (5, 3), (5, 4),
+        coordinates = [(1, 2), (1, 3), (1, 4), (2, 1), (2, 2), (2, 3),
+            (2, 4), (3, 1), (3, 2), (3, 3), (3, 4), (3, 5), (4, 1),
+            (4, 2), (4, 3), (4, 4), (5, 2), (5, 3), (5, 4),
         ]
 
         random.shuffle(tile_types)
@@ -101,7 +143,14 @@ class Game(models.Model):
         tiles = []
         for tile_type in tile_types:
             x, y = coordinates.pop()
-            tiles.append(Tile(game = self, tile_grid_x = x, tile_grid_y = y, type = tile_type))
+            tiles.append(
+                Tile(
+                    game = self,
+                    tile_grid_x = x,
+                    tile_grid_y = y,
+                    type = tile_type
+                )
+            )
             if tile_type != TILE_TYPE['DESERT']:
                 tiles[-1].chit = chits.pop()
 
@@ -109,22 +158,68 @@ class Game(models.Model):
 
     def _distribute_tokens(self):
         for player in self.player_set.all():
-            player.road_set.bulk_create([ Road(player = player) for x in range(15) ])
-            player.city_set.bulk_create([ City(player = player) for x in range(4) ])
-            player.settlement_set.bulk_create([ Settlement(player = player) for x in range(5) ])
+            player.road_set.bulk_create(
+                [Road(player = player) for x in range(15)]
+            )
+            player.city_set.bulk_create(
+                [City(player = player) for x in range(4)]
+            )
+            player.settlement_set.bulk_create(
+                [Settlement(player = player) for x in range(5)]
+            )
+
+    def _generate_development_cards(self):
+        card_types = [DEVELOPMENT_TYPE['KNIGHT']] * 14
+        card_types = card_types + [DEVELOPMENT_TYPE['ROAD_BUILDING']] * 2
+        card_types = card_types + [DEVELOPMENT_TYPE['YEAR_OF_PLENTY']] * 2
+        card_types = card_types + [DEVELOPMENT_TYPE['MONOPOLY']] * 2
+        card_types = card_types + [DEVELOPMENT_TYPE['VICTORY_POINT']] * 5
+        cards = [
+            DevelopmentCard(
+                game = self, type = type
+            ) for type in card_types
+        ]
+        random.shuffle(cards)
+        DevelopmentCard.objects.bulk_create(cards)
+
+    def _generate_resource_cards(self):
+        card_types = [RESOURCE_TYPE['BRICK']] * 19
+        card_types = card_types + [RESOURCE_TYPE['WOOL']] * 19
+        card_types = card_types + [RESOURCE_TYPE['ORE']] * 19
+        card_types = card_types + [RESOURCE_TYPE['GRAIN']] * 19
+        card_types = card_types + [RESOURCE_TYPE['LUMBER']] * 19
+        cards = [
+            ResourceCard(
+                game = self, type = type
+            ) for type in card_types
+        ]
+        random.shuffle(cards)
+        ResourceCard.objects.bulk_create(cards)
 
 class Tile(models.Model):
     game = models.ForeignKey(Game)
     tile_grid_x = models.IntegerField()
     tile_grid_y = models.IntegerField()
-    chit = models.IntegerField(choices = ((x, x) for x in range(2, 7) + range(8, 13)), null = True)
-    type = models.CharField(max_length = 2, choices = ((value, key) for key, value in TILE_TYPE.items()))
+    chit = models.IntegerField(
+        choices = ((x, x) for x in range(2, 13) if x != 7),
+        null = True
+    )
+    type = models.CharField(
+        max_length = 2,
+        choices = ((value, key) for key, value in TILE_TYPE.items())
+    )
 
 class Player(models.Model):
     game = models.ForeignKey(Game)
     user = models.ForeignKey(User, null = True)
     name = models.CharField(max_length = 50)
-    color = models.IntegerField(null = True, choices = ((value, key) for key, value in COLOR.items()))
+    color = models.IntegerField(
+        null = True,
+        choices = ((value, key) for key, value in COLOR.items())
+    )
+
+    class Meta:
+        ordering = ['id']
 
 class Road(models.Model):
     player = models.ForeignKey(Player)
@@ -146,10 +241,27 @@ class Settlement(models.Model):
 class DevelopmentCard(models.Model):
     game = models.ForeignKey(Game)
     player = models.ForeignKey(Player, null = True)
-    used = models.BooleanField(default = False)
-    type = models.CharField(max_length = 2, choices = ((value, key) for key, value in DEVELOPMENT_TYPE.items()))
+    played = models.BooleanField(default = False)
+    type = models.CharField(
+        max_length = 2,
+        choices = ((value, key) for key, value in DEVELOPMENT_TYPE.items())
+    )
 
 class ResourceCard(models.Model):
     game = models.ForeignKey(Game)
     player = models.ForeignKey(Player, null = True)
-    type = models.CharField(max_length = 2, choices = ((value, key) for key, value in RESOURCE_TYPE.items()))
+    type = models.CharField(
+        max_length = 2,
+        choices = ((value, key) for key, value in RESOURCE_TYPE.items())
+    )
+
+class Round(models.Model):
+    game = models.ForeignKey(Game)
+    player = models.ForeignKey(Player)
+    die_1 = models.IntegerField(null = True)
+    die_2 = models.IntegerField(null = True)
+    date_created = models.DateTimeField(auto_now_add = True)
+    date_modified = models.DateTimeField(auto_now = True)
+
+    class Meta:
+        ordering = ['date_created']
--- src/games/apps/settlersOfCatan/tests.py
+++ src/games/apps/settlersOfCatan/tests.py
@@ -2,6 +2,44 @@ from django.test import TestCase
 
 from games.apps.settlersOfCatan.models import *
 
+class Game_add_player(TestCase):
+
+    def setUp(self):
+        self.game = Game.objects.create()
+        self.user1 = User.objects.create(username = 'user1')
+        self.user2 = User.objects.create(username = 'user2')
+
+    def test_players_with_no_user_are_added(self):
+        player1 = self.game.add_player()
+        player2 = self.game.add_player()
+        self.assertNotEqual(player1, player2)
+        self.assertEqual(True, player1 in self.game.player_set.all())
+        self.assertEqual(True, player2 in self.game.player_set.all())
+
+    def test_players_with_user_are_added(self):
+        player1 = self.game.add_player(self.user1)
+        player2 = self.game.add_player(self.user2)
+        self.assertNotEqual(player1, player2)
+        self.assertEqual(True, player1 in self.game.player_set.all())
+        self.assertEqual(True, player2 in self.game.player_set.all())
+
+    def test_users_cannot_be_added_more_than_once(self):
+        player1 = self.game.add_player(self.user1)
+        player2 = self.game.add_player(self.user1)
+        self.assertEqual(player1, player1)
+        self.assertEqual(1, self.game.player_set.count())
+
+    def test_users_not_added_when_status_not_pending(self):
+        self.game.status = STATUS['ACTIVE']
+        player1 = self.game.add_player()
+        self.assertEqual(None, player1)
+        self.assertEqual(0, self.game.player_set.count())
+
+    def test_users_not_added_when_player_limit_exceeded(self):
+        for x in range(10):
+            self.game.add_player()
+        self.assertEqual(4, self.game.player_set.count())
+
 class Game_start(TestCase):
 
     def setUp(self):
@@ -33,3 +71,21 @@ class Game_start(TestCase):
 
     def test_game_is_active(self):
         self.assertEqual(STATUS['ACTIVE'], self.game.status)
+
+    def test_development_cards(self):
+        self.assertEqual(25, self.game.developmentcard_set.count())
+
+    def test_resource_cards(self):
+        self.assertEqual(95, self.game.resourcecard_set.count())
+
+class Game_start_next_round(TestCase):
+
+    def setUp(self):
+        self.game = Game.objects.create()
+        self.player1 = self.game.add_player()
+        self.player2 = self.game.add_player()
+        self.player3 = self.game.add_player()
+        self.game.start()
+
+    def test_player_order(self):
+        pass
