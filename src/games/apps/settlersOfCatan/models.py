0bb866ad961bbdcab9d86b0ca54212ef49ae0f28
ryandeba <ryandeba@gmail.com>
2014-06-08 00:25:06 -0500

    progress on settlers

--- src/games/apps/settlersOfCatan/models.py
+++ src/games/apps/settlersOfCatan/models.py
@@ -1,50 +1,155 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+import random
+
+STATUS = {
+    'PENDING' : 'P',
+    'ACTIVE' : 'A',
+    'FINISHED' : 'F',
+}
+
+TILE_TYPE = {
+    'HILLS' : 'HI',
+    'PASTURE' : 'PA',
+    'MOUNTAINS' : 'MO',
+    'FIELDS' : 'FI',
+    'FOREST' : 'FO',
+    'DESERT' : 'DE',
+}
+
+RESOURCE_TYPE = {
+    'ORE' : 'OR',
+    'WHEAT' : 'WH',
+    'WOOD' : 'WO',
+    'SHEEP' : 'SH',
+    'BRICK' : 'BR',
+}
+
+DEVELOPMENT_TYPE = {
+    'KNIGHT' : 'KN',
+    'ROAD_BUILDING' : 'RB',
+    'YEAR_OF_PLENTY' : 'YP',
+    'MONOPOLY' : 'MO',
+    'VICTORY_POINT' : 'VP',
+}
+
+COLOR = {
+    'RED' : 0,
+    'BLUE' : 1,
+    'YELLOW' : 2,
+    'WHITE' : 3,
+}
+
 class Game(models.Model):
-  date_created = models.DateTimeField(auto_now_add = True)
-  date_modified = models.DateTimeField(auto_now = True)
-  player_largest_army = models.ForeignKey('Player', related_name = '+')
-  player_longest_road = models.ForeignKey('Player', related_name = '+')
-  robber = models.ForeignKey(Hex, related_name = '+')
+    status = models.CharField(max_length = 1, choices = ((value, key) for key, value in STATUS.items()))
+    player_largest_army = models.ForeignKey('Player', related_name = '+', null = True)
+    player_longest_road = models.ForeignKey('Player', related_name = '+', null = True)
+    tile_robber = models.ForeignKey('Tile', related_name = '+', null = True)
+    date_created = models.DateTimeField(auto_now_add = True)
+    date_modified = models.DateTimeField(auto_now = True)
 
-class Hex(models.Model):
-  CHIT_CHOICES = (2,3,4,5,6,8,9,10,11,12)
-  TYPE_CHOICES = ('Hills', 'Pasture', 'Mountains', 'Grain', 'Forest', 'Desert')
+    def __unicode__(self):
+        return """
+                   ---
+                 /     \
+                /       \
+            ---           ---
+          /     \       /     \
+         /       \     /       \
+     ---           ---           ---
+   /     \       /     \       /     \
+  /       \     /       \     /       \
+            ---           ---
+  \       /     \       /     \       /
+   \     /       \     /       \     /
+     ---           ---           ---
+         \       /     \       /
+          \     /       \     /
+            ---           ---
+                \       /
+                 \     /
+                   ---"""
 
-  game = models.ForeignKey(Game)
-  position = models.IntegerField() #TODO: figure out how the position should be stored
-  chit = models.IntegerField(choices = CHIT_CHOICES)
-  type = models.CharField(max_length = 6, choices = TYPE_CHOICES)
+    def add_player(self, user = None):
+        #if self.status != STATUS['PENDING'] and self.player_set.count() < 4:
+        if user == None:
+            return self.player_set.create(game = self)
+        else:
+            return self.player_set.get_or_create(game = self, user = user)
+
+    def start(self):
+        self._generate_board()
+        self._distribute_tokens()
+        self.status = STATUS['ACTIVE']
+        self.save()
+
+    def _generate_board(self):
+        tile_types = [TILE_TYPE['HILLS']] * 3 + [TILE_TYPE['PASTURE']] * 4 + [TILE_TYPE['MOUNTAINS']] * 3 + [TILE_TYPE['FIELDS']] * 4 + [TILE_TYPE['FOREST']] * 4 + [TILE_TYPE['DESERT']]
+        chits = [2,3,3,4,4,5,5,6,6,8,8,9,9,10,10,11,11,12]
+        coordinates = [
+            (1, 2), (1, 3), (1, 4),
+            (2, 1), (2, 2), (2, 3), (2, 4),
+            (3, 1), (3, 2), (3, 3), (3, 4), (3, 5),
+            (4, 1), (4, 2), (4, 3), (4, 4),
+            (5, 2), (5, 3), (5, 4),
+        ]
+
+        random.shuffle(tile_types)
+        random.shuffle(chits)
+
+        tiles = []
+        for tile_type in tile_types:
+            x, y = coordinates.pop()
+            tiles.append(Tile(game = self, tile_grid_x = x, tile_grid_y = y, type = tile_type))
+            if tile_type != TILE_TYPE['DESERT']:
+                tiles[-1].chit = chits.pop()
+
+        self.tile_set.bulk_create(tiles)
+
+    def _distribute_tokens(self):
+        for player in self.player_set.all():
+            player.road_set.bulk_create([ Road(player = player) for x in range(15) ])
+            player.city_set.bulk_create([ City(player = player) for x in range(4) ])
+            player.settlement_set.bulk_create([ Settlement(player = player) for x in range(5) ])
+
+class Tile(models.Model):
+    game = models.ForeignKey(Game)
+    tile_grid_x = models.IntegerField()
+    tile_grid_y = models.IntegerField()
+    chit = models.IntegerField(choices = ((x, x) for x in range(2, 7) + range(8, 13)), null = True)
+    type = models.CharField(max_length = 2, choices = ((value, key) for key, value in TILE_TYPE.items()))
 
 class Player(models.Model):
-  COLOR_CHOICES = ('Red', 'Blue', 'White', 'Yellow')
+    game = models.ForeignKey(Game)
+    user = models.ForeignKey(User, null = True)
+    name = models.CharField(max_length = 50)
+    color = models.IntegerField(null = True, choices = ((value, key) for key, value in COLOR.items()))
 
-  game = models.ForeignKey(Game)
-  user = models.ForeignKey(User, null = True)
-  name = models.CharField(max_length = 50)
-  color = models.CharField(max_length = 6, choices = COLOR_CHOICES)
+class Road(models.Model):
+    player = models.ForeignKey(Player)
+    start_point_grid_x = models.IntegerField(null = True)
+    start_point_grid_y = models.IntegerField(null = True)
+    end_point_grid_x = models.IntegerField(null = True)
+    end_point_grid_y = models.IntegerField(null = True)
 
-class Road(models.Model): #each player gets 15
-  player = models.ForeignKey(Player)
+class City(models.Model):
+    player = models.ForeignKey(Player)
+    point_grid_x = models.IntegerField(null = True)
+    point_grid_y = models.IntegerField(null = True)
 
-class City(models.Model): #each player gets 4
-  player = models.ForeignKey(Player)
-
-class Settlement(models.Model): #each player gets 5
-  player = models.ForeignKey(Player)
+class Settlement(models.Model):
+    player = models.ForeignKey(Player)
+    point_grid_x = models.IntegerField(null = True)
+    point_grid_y = models.IntegerField(null = True)
 
 class DevelopmentCard(models.Model):
-  TYPE_CHOICES = ('Knight', 'Road Building', 'Year of plenty', 'Monopoly', 'Victory Point')
-
-  game = models.ForeignKey(Game)
-  player = models.ForeignKey(Player, null = True)
-  used = models.BooleanField(default = False)
-  type = models.CharField(max_length = 20, choices = TYPE_CHOICES)
+    game = models.ForeignKey(Game)
+    player = models.ForeignKey(Player, null = True)
+    used = models.BooleanField(default = False)
+    type = models.CharField(max_length = 2, choices = ((value, key) for key, value in DEVELOPMENT_TYPE.items()))
 
 class ResourceCard(models.Model):
-  TYPE_CHOICES = ('Ore', 'Wheat', 'Wood', 'Sheep', 'Brick')
-
-  game = models.ForeignKey(Game)
-  player = models.ForeignKey(Player, null = True)
-  type = models.CharField(max_length = 6, choices = TYPE_CHOICES)
+    game = models.ForeignKey(Game)
+    player = models.ForeignKey(Player, null = True)
+    type = models.CharField(max_length = 2, choices = ((value, key) for key, value in RESOURCE_TYPE.items()))
--- src/games/apps/settlersOfCatan/tests.py
+++ src/games/apps/settlersOfCatan/tests.py
@@ -2,7 +2,34 @@ from django.test import TestCase
 
 from games.apps.settlersOfCatan.models import *
 
-class GameTests(TestCase):
+class Game_start(TestCase):
 
-  def test_nothing(self):
-    self.assertEqual(True, True)
+    def setUp(self):
+        self.game = Game.objects.create()
+        self.game.add_player()
+        self.game.add_player()
+        self.game.start()
+
+    def test_tiles_are_created(self):
+        self.assertEqual(19, self.game.tile_set.count())
+
+    def test_roads_are_distributed(self):
+        player1 = self.game.player_set.all()[0]
+        player2 = self.game.player_set.all()[1]
+        self.assertEqual(15, player1.road_set.count())
+        self.assertEqual(15, player2.road_set.count())
+
+    def test_cities_are_distributed(self):
+        player1 = self.game.player_set.all()[0]
+        player2 = self.game.player_set.all()[1]
+        self.assertEqual(4, player1.city_set.count())
+        self.assertEqual(4, player2.city_set.count())
+
+    def test_settlements_are_distributed(self):
+        player1 = self.game.player_set.all()[0]
+        player2 = self.game.player_set.all()[1]
+        self.assertEqual(5, player1.settlement_set.count())
+        self.assertEqual(5, player2.settlement_set.count())
+
+    def test_game_is_active(self):
+        self.assertEqual(STATUS['ACTIVE'], self.game.status)
